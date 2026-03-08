f51d9d4247fcbf4fc18b9153d9ac14c7578605fe
Samuel Davis <samueljakdavis@gmail.com>
2026-03-08 16:25:18 -0400

    app types (abstract of api); utilities for nested types to create consistent data access apis

--- src/Types/index.ts
+++ src/Types/index.ts
@@ -12,6 +12,113 @@ import type {
   TVSeriesId,
 } from "./TMDB";
 import type { EdgeDataDefinition, NodeDataDefinition } from "cytoscape";
+import { createStore, produce } from "solid-js/store";
+
+export type MapLike<T extends { id: PropertyKey }> = {
+  has(id: T["id"]): boolean;
+  get(id: T["id"]): undefined | T;
+  set(item: T): T;
+  del(id: T["id"]): undefined | T;
+  arr(): T[];
+};
+
+export function createMapLike<
+  K extends PropertyKey,
+  T extends { id: K },
+>(): ReturnType<
+  typeof createStore<{ items: Partial<Record<T["id"], T>> } & MapLike<T>>
+> {
+  const [state, setState] = createStore<
+    { items: Partial<Record<T["id"], T>> } & MapLike<T>
+  >({
+    items: {},
+    has(id) {
+      return id in this.items;
+    },
+    get(id) {
+      return this.items[id];
+    },
+    set(item) {
+      setState(
+        produce((state) => {
+          state.items[item.id] = item;
+        }),
+      );
+      return item;
+    },
+    del(id) {
+      let item = this.get(id);
+      setState(
+        produce((state) => {
+          delete state.items[id];
+        }),
+      );
+      return item;
+    },
+    arr() {
+      return Object.values(this.items);
+    },
+  });
+  return [state, setState];
+}
+
+export type SetLike<T extends PropertyKey> = {
+  has(item: T): boolean;
+  add(item: T): void;
+  del(item: T): void;
+  arr(): T[];
+};
+
+export function createSetLike<T extends PropertyKey>(): ReturnType<
+  typeof createStore<{ items: T[] } & SetLike<T>>
+> {
+  const [state, setState] = createStore<{ items: T[] } & SetLike<T>>({
+    items: [],
+    has(id) {
+      return this.items.includes(id);
+    },
+    add(id) {
+      setState(
+        produce((state) => {
+          if (!this.has(id)) state.items.push(id);
+        }),
+      );
+    },
+    del(id) {
+      setState(
+        produce(
+          (state) => (state.items = state.items.filter((item) => item !== id)),
+        ),
+      );
+    },
+    arr() {
+      return this.items;
+    },
+  });
+  return [state, setState];
+}
+
+export type Person = {
+  id: PersonId;
+  name: string;
+  profile_path: ImgPath;
+};
+export type TvSeries = {
+  id: TVSeriesId;
+  name: string;
+  poster_path: ImgPath;
+};
+export type Credit<D extends Department = Department> = {
+  id: CreditId;
+  department: D;
+  job: Job<D>;
+};
+export type Edge = {
+  id: CreditId;
+  tvSeriesId: TVSeriesId;
+  personId: PersonId;
+  creditId: CreditId;
+};
 
 export type AppTvSeriesSearch = Pick<
   SearchTVResponseResult,
